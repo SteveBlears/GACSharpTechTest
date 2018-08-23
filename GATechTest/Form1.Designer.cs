b2445ecb372c7512909b56db0386ba532c6a6546
Steve Blears <steve_blears@hotmail.com>
2018-08-23 09:48:30 +0100

    Initial commit

---
+++ GATechTest/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace GATechTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Calculate = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(132, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(240, 20);
+            this.textBox1.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(71, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Enter text:";
+            // 
+            // Calculate
+            // 
+            this.Calculate.Location = new System.Drawing.Point(16, 55);
+            this.Calculate.Name = "Calculate";
+            this.Calculate.Size = new System.Drawing.Size(75, 23);
+            this.Calculate.TabIndex = 14;
+            this.Calculate.Text = "Calculate";
+            this.Calculate.UseVisualStyleBackColor = true;
+            this.Calculate.Click += new System.EventHandler(this.Calculate_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Key Sequence:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(132, 99);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(240, 20);
+            this.textBox2.TabIndex = 16;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(132, 142);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(90, 20);
+            this.textBox3.TabIndex = 17;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 13);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Minimum time required:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(416, 182);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Calculate);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Calculate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ GATechTest/Form1.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GATechTest
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        double keyPress = .100;
+        double keyPause = .5;
+        double minumumDuration = 0;
+        private void Calculate_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "";
+            textBox3.Text = "";
+            StringBuilder keySequence = new StringBuilder();
+            bool keyPaused;
+            bool validkey;
+            char[] stringToAnalyse = textBox1.Text.ToUpper().ToCharArray();
+            int i = 0;
+            int keysPressed = 0;
+            string input = "";
+            while (i < stringToAnalyse.Length )
+            {
+                keyPaused = false;
+                validkey = true;
+                switch (stringToAnalyse[i])
+                {
+                    case 'A':
+                        input = "2";
+                        keysPressed = 1;
+                        if (keySequence.Length> 0 && keySequence.ToString().EndsWith("2"))
+                        {
+                            keyPaused = true;
+                        }
+                        break;
+                    case 'B':
+                        input = "22";
+                        keysPressed = 2;
+                        if (keySequence.Length > 0 && keySequence.ToString().EndsWith("2"))
+                        {
+                            keyPaused = true;
+                        }
+                        break;
+                    case 'C':
+                        input = "222";
+                        keysPressed = 3;
+                        if (keySequence.Length > 0 && keySequence.ToString().EndsWith("2"))
+                        {
+                            keyPaused = true;
+                        }
+                        break;
+                    case 'D':
+                        input = "3";
+                        keysPressed = 1;
+                        if (keySequence.Length > 0 && keySequence.ToString().EndsWith("3"))
+                        {
+                            keyPaused = true;
+                        }
+                        break;
+                    case 'E':
+                        input = "33";
+                        keysPressed = 2;
+                        if (keySequence.Length > 0 && keySequence.ToString().EndsWith("3"))
+                        {
+                            keyPaused = true;
+                        }
+                        break;
+                    case 'F':
+                        input = "333";
+                        keysPressed = 3;
+                        if (keySequence.Length > 0 && keySequence.ToString().EndsWith("3"))
+                        {
+                            keyPaused = true;
+                        }
+                        break;
+                    case 'G':
+                        input = "4";
+                        keysPressed = 1;
+                        if (keySequence.Length > 0 && keySequence.ToString().EndsWith("4"))
+                        {
+                            keyPaused = true;
+                        }
+                        break;
+                    case 'H':
+                        input = "44";
+                        keysPressed = 2;
+                        if (keySequence.Length > 0 && keySequence.ToString().EndsWith("4"))
+                        {
+                            keyPaused = true;
+                        }
+                        break;
+                    case 'I':
+                        input = "444";
+                        keysPressed = 3;
+                        if (keySequence.Length > 0 && keySequence.ToString().EndsWith("4"))
+                        {
+                            keyPaused = true;
+                        }
+                        break;
+                    case 'J':
+                        input = "5";
+                        keysPressed = 1;
+                        if (keySequence.Length > 0 && keySequence.ToString().EndsWith("5"))
+                        {
+                            keyPaused = true;
+                        }
+                        break;
+                    case 'K':
+                        input = "55";
+                        keysPressed = 2;
+                        if (keySequence.Length > 0 && keySequence.ToString().EndsWith("5"))
+                        {
+                            keyPaused = true;
+                        }
+                        break;
+                    case 'L':
+                        input = "555";
+                        keysPressed = 3;
+                        if (keySequence.Length > 0 && keySequence.ToString().EndsWith("5"))
+                        {
+                            keyPaused = true;
+                        }
+                        break;
+                    case 'M':
+                        input = "6";
+                        keysPressed = 1;
+                        if (keySequence.Length > 0 && keySequence.ToString().EndsWith("6"))
+                        {
+                            keyPaused = true;
+                        }
+                        break;
+                    case 'N':
+                        input = "66";
+                        keysPressed = 2;
+                        if (keySequence.Length > 0 && keySequence.ToString().EndsWith("6"))
+                        {
+                            keyPaused = true;
+                        }
+                        break;
+                    case 'O':
+                        input = "666";
+                        keysPressed = 3;
+                        if (keySequence.Length > 0 && keySequence.ToString().EndsWith("6"))
+                        {
+                            keyPaused = true;
+                        }
+                        break;
+                    case 'P':
+                        input = "7";
+                        keysPressed = 1;
+                        if (keySequence.Length > 0 && keySequence.ToString().EndsWith("7"))
+                        {
+                            keyPaused = true;
+                        }
+                        break;
+                    case 'Q':
+                        input = "77";
+                        keysPressed = 2;
+                        if (keySequence.Length > 0 && keySequence.ToString().EndsWith("7"))
+                        {
+                            keyPaused = true;
+                        }
+                        break;
+                    case 'R':
+                        input = "777";
+                        keysPressed = 3;
+                        if (keySequence.Length > 0 && keySequence.ToString().EndsWith("7"))
+                        {
+                            keyPaused = true;
+                        }
+                        break;
+                    case 'S':
+                        input = "7777";
+                        keysPressed = 4;
+                        if (keySequence.Length > 0 && keySequence.ToString().EndsWith("7"))
+                        {
+                            keyPaused = true;
+                        }
+                        break;
+                    case 'T':
+                        input = "8";
+                        keysPressed = 1;
+                        if (keySequence.Length > 0 && keySequence.ToString().EndsWith("8"))
+                        {
+                            keyPaused = true;
+                        }
+                        break;
+                    case 'U':
+                        input = "88";
+                        keysPressed = 2;
+                        if (keySequence.Length > 0 && keySequence.ToString().EndsWith("8"))
+                        {
+                            keyPaused = true;
+                        }
+                        break;
+                    case 'V':
+                        input = "888";
+                        keysPressed = 3;
+                        if (keySequence.Length > 0 && keySequence.ToString().EndsWith("8"))
+                        {
+                            keyPaused = true;
+                        }
+                        break;
+                    case 'W':
+                        input = "9";
+                        keysPressed = 1;
+                        if (keySequence.Length > 0 && keySequence.ToString().EndsWith("9"))
+                        {
+                            keyPaused = true;
+                        }
+                        break;
+                    case 'X':
+                        input = "99";
+                        keysPressed = 2;
+                        if (keySequence.Length > 0 && keySequence.ToString().EndsWith("9"))
+                        {
+                            keyPaused = true;
+                        }
+                        break;
+                    case 'Y':
+                        input = "999";
+                        keysPressed = 3;
+                        if (keySequence.Length > 0 && keySequence.ToString().EndsWith("9"))
+                        {
+                            keyPaused = true;
+                        }
+                        break;
+                    case 'Z':
+                        input = "9999";
+                        keysPressed = 4;
+                        if (keySequence.Length > 0 && keySequence.ToString().EndsWith("9"))
+                        {
+                            keyPaused = true;
+                        }
+                        break;
+                    case ' ':
+                        input = "0";
+                        keysPressed = 1;
+                        if (keySequence.Length > 0 && keySequence.ToString().EndsWith("0"))
+                        {
+                            keyPaused = true;
+                        }
+                        break;
+                    default:
+                        validkey = false;
+                        input = "";
+                        break;
+                 
+               }
+                if (validkey)
+                {
+                    minumumDuration = minumumDuration + (keyPress * keysPressed);
+                    if (keyPaused)
+                    {
+                        minumumDuration = minumumDuration + keyPause;
+                    }
+                }
+                keySequence.Append(input);
+                i++;
+            }
+            textBox2.Text = keySequence.ToString();
+            textBox3.Text = minumumDuration.ToString();
+        }
+
+
+    }
+}
